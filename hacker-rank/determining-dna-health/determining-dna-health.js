c4a68ec7beef40717c1deccdb0ad8637600e55bd
Alex Kovar <ajkovar@gmail.com>
2021-03-17 20:03:31 -0600

    dna health

---
+++ hacker-rank/determining-dna-health/determining-dna-health.js
@@ -0,0 +1,62 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map((str) => str.replace(/\s*$/, ""));
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function getHealthValue({ genes, health, first, last, sequence }) {
+  let value = 0;
+  let allMatch;
+  for (var i = first; i <= last; i++) {
+    value += (sequence.match(new RegExp(`(?=${genes[i]})`, "g")) || []).length * health[i];
+  }
+  return value;
+}
+
+function main() {
+  const n = parseInt(readLine(), 10);
+
+  const genes = readLine().split(" ");
+
+  const health = readLine()
+    .split(" ")
+    .map((healthTemp) => parseInt(healthTemp, 10));
+
+  const s = parseInt(readLine(), 10);
+
+  let max = 0;
+  let min = Infinity;
+  for (let sItr = 0; sItr < s; sItr++) {
+    const firstLastd = readLine().split(" ");
+
+    const first = parseInt(firstLastd[0], 10);
+
+    const last = parseInt(firstLastd[1], 10);
+
+    const d = firstLastd[2];
+    const value = getHealthValue({ genes, health, first, last, sequence: d });
+
+    max = Math.max(max, value);
+    min = Math.min(min, value);
+  }
+  console.log([min === Infinity ? 0 : min, max].join(" "));
+}
