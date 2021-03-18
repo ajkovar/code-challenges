9a791b1f59316021762c95da4afbfae5ee222bee
Alex Kovar <ajkovar@gmail.com>
2021-03-17 23:38:36 -0600

    scanl instead of uniq

--- hacker-rank/climbing-the-leaderboard/Main.hs
+++ hacker-rank/climbing-the-leaderboard/Main.hs
@@ -2,28 +2,23 @@ import Data.List (group)
 import Control.Monad (void)
 import Data.Foldable (find)
 
-uniq :: Eq a => [a] -> [a]
-uniq = uniq' []  
-  where 
-    uniq' xs' [] = []
-    uniq' xs' (x:xs) = 
-      if x `elem` xs'
-        then uniq' xs' xs 
-        else x:uniq' (x:xs') xs
+assignRanks :: (Num a1, Ord a2) => [a2] -> [(a2, a1)]
+assignRanks [] = []
+assignRanks a = scanl (\(last, place) i -> (i, place + (if last > i then 1 else 0))) (head a, 1) a
 
 climbingLeaderboard :: [Int] -> [Int] -> String
-climbingLeaderboard ranked player = 
-  unlines $ fmap (show . rank) player 
+climbingLeaderboard ranked player =
+  unlines $ fmap (show . rank) player
   where
-    ranked' = zip (uniq ranked) [1 .. ]
+    ranked' = assignRanks ranked
     rank i = case find ((<= i) . fst) ranked' of
-      Nothing -> length ranked' + 1
+      Nothing -> snd (last ranked') + 1
       Just x -> snd x + (if i >= fst x then 0 else 1)
 
 main :: IO ()
 main = do
   contents <- getContents
-  let lns = lines contents 
+  let lns = lines contents
   let ranked = toScores $ lns !! 1
   let player = toScores $ lns !! 3
   void $ putStrLn $ climbingLeaderboard ranked player
