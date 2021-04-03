a7dd5719eac12b158ce7beb7455d79062177c9e7
Alex Kovar <ajkovar@gmail.com>
2021-04-03 05:47:29 -0600

    fibonacci modified

---
+++ hacker-rank/fibonacci-modified/Main.hs
@@ -0,0 +1,7 @@
+import Data.Foldable (foldl')
+fibonacciModified :: Integer -> Integer -> Integer -> Integer
+fibonacciModified t1 t2 n = snd $ foldl' (\(a, b) _ -> (b, a + b ^ 2)) (t1,  t2) [1..n-2]
+
+main :: IO ()
+main = do
+  getLine >>= print . (\[t1, t2, n] -> fibonacciModified t1 t2 n) . fmap read . words
