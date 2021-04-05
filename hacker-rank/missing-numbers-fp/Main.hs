c1a21806c43c50faf3a662429ef0786ffe784417
Alex Kovar <ajkovar@gmail.com>
2021-04-05 12:33:24 -0500

    working version of missing numbers

--- hacker-rank/missing-numbers-fp/Main.hs
+++ hacker-rank/missing-numbers-fp/Main.hs
@@ -1,11 +1,18 @@
-import Data.List ((\\), sort)
 import Data.Functor ((<&>))
+import Data.IntMap (IntMap, insertWith, empty, findWithDefault, foldrWithKey)
+import Data.IntSet (IntSet, empty, toList, insert)
 
 getArray :: IO [Int]
 getArray = getLine >> getLine <&> (fmap read . words)
 
+toMap :: [Int] -> IntMap Int
+toMap = foldl (\s i -> insertWith (+) i 1 s) Data.IntMap.empty
+
+difference :: IntMap Int -> IntMap Int -> IntSet
+difference a b = foldrWithKey (\k a' s -> if findWithDefault 0 k b > a' then insert k s else s) Data.IntSet.empty a
+
 main :: IO ()
 main = do
-  a <-  getArray
-  b <- getArray
-  putStrLn $ unwords $ show <$> sort (b \\ a)
+  a <-  toMap <$> getArray
+  b <- toMap <$> getArray
+  putStrLn $ unwords $ show <$> toList (difference a b)
