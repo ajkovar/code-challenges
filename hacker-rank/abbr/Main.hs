18bb0d5eba932bfccd8a8c312c48378630de1285
Alex Kovar <ajkovar@gmail.com>
2021-04-14 17:21:38 -0500

    almost working solution for abbr

--- hacker-rank/abbr/Main.hs
+++ hacker-rank/abbr/Main.hs
@@ -1,17 +1,18 @@
 import Control.Monad (replicateM_)
 import Data.Char (toUpper, isUpper)
 
-abbreviation :: [Char] -> [Char] -> [Char]
+abbreviation :: [Char] -> [Char] -> Bool
 abbreviation as [] 
-  | any isUpper as = "NO"
-  | otherwise  = "YES"
-abbreviation [] _ = "NO" 
-abbreviation (a:as) (b:bs) 
-  | a == b || toUpper a == b = abbreviation as bs
-  | isUpper a = "NO"
+  | any isUpper as = False
+  | otherwise  = True
+abbreviation [] _ = False 
+abbreviation (a:as) (b:bs)
+  | a == b = abbreviation as bs
+  | toUpper a == b = abbreviation as bs || abbreviation as (b:bs)
+  | isUpper a = False
   | otherwise = abbreviation as (b:bs)
 
 main :: IO ()
 main = do
   n <- read <$> getLine 
-  replicateM_ n (abbreviation <$> getLine <*> getLine >>= putStrLn) 
+  replicateM_ n (abbreviation <$> getLine <*> getLine >>= (\a -> putStrLn (if a then "YES" else "NO"))) 
