ca5dd9673d7a1927cd9b9f5c412b25aaaec94eff
Alex Kovar <ajkovar@gmail.com>
2021-04-27 14:37:56 -0500

    initial version of sort

--- hacker-rank/countingsort4/Main.hs
+++ hacker-rank/countingsort4/Main.hs
@@ -1,22 +1,37 @@
-import Control.Monad
-import Data.Array
-import Data.Array.ST
-import Data.List (sortBy)
+{-# LANGUAGE TupleSections #-}
+
+import Control.Monad as C
+import Control.Monad.ST
+import Data.Vector as V
+import Data.Vector.Mutable as M
+import Debug.Trace;
 
 parseList :: IO [([Char], Int)]
 parseList = do
-  n <- (`div` 2) . read <$> getLine
+  n <- (`div` 2) . Prelude.read <$> getLine
   array <- fmap words . lines <$> getContents
-  return $ zipWith (\[j, word] i -> (if i < n then "-" else word, read j)) array [0 ..]
+  return $ Prelude.zipWith (\[j, word] i -> (if i < n then "-" else word, Prelude.read j)) array [0 ..]
 
-createArray :: [([Char], Int)] -> Array Int (Int, Int)
-createArray list = runSTArray $ do
-  let max = maximum (snd <$> list)
-  array <- newArray (0, max) (0, 0)
-  forM_ list $ \(word, i) -> do
-    (_, count) <- readArray array i
-    writeArray array i (count + 1, count + 1)
-  return array
+-- createArray :: [([Char], Int)] -> [String]
+createArray list = runST $ do
+  v <- M.replicate max 0
+  C.forM_ list $ \(word, i) -> do
+    count <- M.read v i
+    M.write v i (count + 1)
+
+  v2 <- V.unsafeFreeze v >>= V.unsafeThaw . fmap (,0) . V.scanl1 (+)
+  (max2, _) <- M.read v2 (max -1)
+
+  v3 <- M.new max2
+  C.forM_ list $ \(word, i) -> do
+    (max, count) <- M.read v2 i
+    
+    M.modify v2 (fmap (+1)) i
+    trace (word <> " " <> show max <> " " <> show count) M.write v3 (max-count-1) word
+
+  toList <$> V.freeze v3
+  where
+    max = Prelude.maximum (snd <$> list) + 1
 
 main :: IO ()
 main = do
