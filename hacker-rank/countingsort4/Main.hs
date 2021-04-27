edb06e378a90e54e258bc5d2fb134379054f6ba5
Alex Kovar <ajkovar@gmail.com>
2021-04-27 16:27:58 -0500

    working version of couting sort

--- hacker-rank/countingsort4/Main.hs
+++ hacker-rank/countingsort4/Main.hs
@@ -1,10 +1,7 @@
-{-# LANGUAGE TupleSections #-}
-
 import Control.Monad as C
 import Control.Monad.ST
 import Data.Vector as V
 import Data.Vector.Mutable as M
-import Debug.Trace;
 
 parseList :: IO [([Char], Int)]
 parseList = do
@@ -12,22 +9,22 @@ parseList = do
   array <- fmap words . lines <$> getContents
   return $ Prelude.zipWith (\[j, word] i -> (if i < n then "-" else word, Prelude.read j)) array [0 ..]
 
--- createArray :: [([Char], Int)] -> [String]
+createArray :: [([Char], Int)] -> [String]
 createArray list = runST $ do
   v <- M.replicate max 0
   C.forM_ list $ \(word, i) -> do
     count <- M.read v i
     M.write v i (count + 1)
 
-  v2 <- V.unsafeFreeze v >>= V.unsafeThaw . fmap (,0) . V.scanl1 (+)
+  frozen <- V.unsafeFreeze v
+  v2 <- V.unsafeThaw $ V.zip (V.scanl1 (+) frozen) frozen
   (max2, _) <- M.read v2 (max -1)
 
   v3 <- M.new max2
   C.forM_ list $ \(word, i) -> do
-    (max, count) <- M.read v2 i
-    
-    M.modify v2 (fmap (+1)) i
-    trace (word <> " " <> show max <> " " <> show count) M.write v3 (max-count-1) word
+    (index, delta) <- M.read v2 i
+    M.modify v2 (fmap (\i -> i - 1)) i
+    M.write v3 (index-delta) word
 
   toList <$> V.freeze v3
   where
@@ -36,6 +33,5 @@ createArray list = runST $ do
 main :: IO ()
 main = do
   list <- parseList
-  -- print $ sortBy (\a b -> compare (snd a) (snd b)) list
-  print $ createArray list
+  putStrLn $ unwords $ createArray list
   return ()
